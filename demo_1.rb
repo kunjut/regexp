28baa6d28dd14de407fb4a589663a6af8ec89a3e
Vadim Dimnich <v.dimnich@gmail.com>
2019-11-12 16:46:42 +0300

    re via function

--- demo_1.rb
+++ demo_1.rb
@@ -1,15 +1,15 @@
 word1 = 'AutoMechanic'
-word2 = 'Automobile'
+word2 = 'Автоmobile'
 word3 = 'Architecture'
 word4 = 'Bobcats'
 
 def check_strings_via_regexp (string1, string2)
   if string1 =~ /^#{string2}/
-    puts "Match it's find"
+    puts "Match it's find по-русски"
   else
     puts 'No match'
   end
 end
 
 check_strings_via_regexp(word1, "Auto")
-check_strings_via_regexp(word2, "Auto")
+check_strings_via_regexp(word2, "Авто")
