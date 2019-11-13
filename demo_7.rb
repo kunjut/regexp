eeacfc164ce245387c315864ad36d9ee03832645
Vadim Dimnich <v.dimnich@gmail.com>
2019-11-13 16:08:59 +0300

    .match instead =~

--- demo_7.rb
+++ demo_7.rb
@@ -2,9 +2,9 @@ pattern = /Ruby/
 pattern2 = /(\w+ )+/
 text = "in system Ruby 2.4.4 installed"
 
-# if data = pattern.match(text)
-#   print data.pre_match
-# end
+if data = pattern.match(text)
+  p data.pre_match
+end
 
 pattern2.match(text) do |item|
   p item.to_s
